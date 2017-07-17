32d6df1d115bd8e78e1d9fb02687966c6dbcd830
DESKTOP-NJU2M2R\pariy <pariyathida@gmail.com>
2017-07-18 01:50:00 +0700

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './providers/auth.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  // private isLoggedIn: Boolean;
+  constructor(public authService: AuthService, private router: Router){
+    // this.authService.authState.subscribe(
+    //   (auth) => {
+    //     if (auth == null) {
+    //       // not logged in
+    //       this.isLoggedIn = false;
+    //       this.router.navigate(['login']);
+    //     } else {
+    //       // logged in
+    //       this.isLoggedIn = true;
+    //       this.router.navigate(['']);
+    //     }
+    //   }
+    // );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes} from '@angular/router';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
+import { AuthService } from './providers/auth.service';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
 
 export const firebaseConfig = {
-  https://medium.com/codingthesmartway-com-blog/angular-2-firebase-authentication-7593c565fa53
-  17.20 mins
-}
+    apiKey: 'AIzaSyBw7XI7wxhmhIIZDdeeSlQCNSpp1RqGhPg',
+    authDomain: 'smartfridge-40cfd.firebaseapp.com',
+    databaseURL: 'https://smartfridge-40cfd.firebaseio.com',
+    projectId: 'smartfridge-40cfd',
+    storageBucket: 'smartfridge-40cfd.appspot.com',
+    messagingSenderId: '1051575034709'
+};
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
+    RegisterPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
