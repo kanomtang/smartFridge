50465fa71341f8946b0d74d62ef4b30331699f2f
kanomtang <rungroj_s@cmu.ac.th>
2017-07-27 20:01:09 +0700

    Change the return type of method inside product.ts and user.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -25,89 +25,112 @@ export class ProductComponent {
 
   }
 
-  deleteItem(keyparam: string): void {
+  deleteItem(keyparam: string): string {
     // delete single item
     // the first slot is path or child
     // the second is key
-    const pathFirebase = 'ProductInfo/' + keyparam;
-    this.af.object(pathFirebase)
-      .remove()
-      .then(() => alert('Successful for deleting item '));
+    try{
+      const pathFirebase = 'ProductInfo/' + keyparam;
+      this.af.object(pathFirebase)
+        .remove()
+        .then(() => alert('Successful for deleting item '));
 
+      return keyparam + 'has been deleted'
+    }catch (err) {
+      return 'error'
+    }
   }
 
-  updateItem(keyparam: string): void {
+  updateItem(keyparam: string): string {
     // this.af.object('Item/{key}').update({'name': 'Jasmine' } );
-    this.onEdit();
-    this.datetime = this.currentdate.getDate() + '/'
-      + (this.currentdate.getMonth() + 1 ) + '/'
-      + this.currentdate.getFullYear() + ' @ '
-      + this.currentdate.getHours() + ':'
-      + this.currentdate.getMinutes() + ':'
-      + this.currentdate.getSeconds();
-    const pathFirebase = 'ProductInfo/' + keyparam;
-    this.af.object(pathFirebase)
-      .update({'Price': this.model.Price,
+    try{
+      this.onEdit();
+      this.datetime = this.currentdate.getDate() + '/'
+        + (this.currentdate.getMonth() + 1 ) + '/'
+        + this.currentdate.getFullYear() + ' @ '
+        + this.currentdate.getHours() + ':'
+        + this.currentdate.getMinutes() + ':'
+        + this.currentdate.getSeconds();
+      const pathFirebase = 'ProductInfo/' + keyparam;
+      this.af.object(pathFirebase)
+        .update({'Price': this.model.Price,
+
+          'ProductName': this.model.ProductName,
+          'LastUpdate' : this.model.LastUpdate = this.datetime,
+          'InUse': this.model.InUse} )
+        .then(() => alert('Successful for Updating '));
+      this.editKey = null;
+
+      return keyparam + 'has been update'
+    }catch (err) {
+      return 'error'
+    }
+  }
+
+  addItem(): string {
+    //   อาจจะรับมาเป็น Product type ในหน้า html คงเป็น item  จสกนนั้นใน method ก็เขียนว่า 'CreatedDate' : productparam.CreatedDate
+    try{
+      this.datetime = this.currentdate.getDate() + '/'
+        + (this.currentdate.getMonth() + 1 ) + '/'
+        + this.currentdate.getFullYear() + ' @ '
+        + this.currentdate.getHours() + ':'
+        + this.currentdate.getMinutes() + ':'
+        + this.currentdate.getSeconds();
+
+      this.items.push({
+        'InUse': this.model.InUse = true,
+        'Price': this.model.Price,
 
         'ProductName': this.model.ProductName,
-        'LastUpdate' : this.model.LastUpdate = this.datetime,
-        'InUse': this.model.InUse} )
-      .then(() => alert('Successful for Updating '));
-    this.editKey = null;
-  }
-
-  addItem(): void {
-    //   อาจจะรับมาเป็น Product type ในหน้า html คงเป็น item  จสกนนั้นใน method ก็เขียนว่า 'CreatedDate' : productparam.CreatedDate
-    this.datetime = this.currentdate.getDate() + '/'
-      + (this.currentdate.getMonth() + 1 ) + '/'
-      + this.currentdate.getFullYear() + ' @ '
-      + this.currentdate.getHours() + ':'
-      + this.currentdate.getMinutes() + ':'
-      + this.currentdate.getSeconds();
-
-    this.items.push({
-      'InUse': this.model.InUse = true,
-      'Price': this.model.Price,
-
-      'ProductName': this.model.ProductName,
-      'CreatedDate': this.model.CreatedDate = this.datetime,
-      'LastUpdate' : this.model.LastUpdate = this.datetime
-    })
-      .then(
-        () => alert('Successful for adding new item')
-      );
-    this.model.InUse= null;
-    this.model.ProductName = null;
-    this.model.Price = null;
-    this.model.CreatedDate= null;
-    this.onAdding();
+        'CreatedDate': this.model.CreatedDate = this.datetime,
+        'LastUpdate' : this.model.LastUpdate = this.datetime
+      })
+        .then(
+          () => alert('Successful for adding new item')
+        );
+      this.model.InUse= null;
+      this.model.ProductName = null;
+      this.model.Price = null;
+      this.model.CreatedDate= null;
+      this.onAdding();
+      return   'item has been added'
+    }catch (err) {
+      return 'error'
+    }
   }
 
 
-  onEdit(): void {
+  onEdit(): string {
     // this method for change the state of isEdit that can disabled the button Edit or Save
     if (this.isEdit === true) {
       this.isEdit = false;
     } else {
       this.isEdit = true;
+      return 'isEdit = true'
     }
+    return 'isEdit = false'
   }
 
-  onAdding(): void {
+  onAdding(): string {
     // this method for enable the user to adding the new product
     if (this.isAdd === true) {
       this.isAdd = false;
     } else {
       this.isAdd = true;
+      return 'isAdd = true'
     }
+    return 'isAdd = false'
   }
 
 
 
-  openToEdit(keyparam: string): void {
+  openToEdit(keyparam: string): string {
     this.onEdit();
     this.editKey = keyparam;
+
+    return 'editKey=' + keyparam;
   }
+
 
 
 }
--- src/app/user-component/user-component.component.ts
+++ src/app/user-component/user-component.component.ts
@@ -27,93 +27,116 @@ export class UserComponentComponent {
 
   }
 
-  deleteItem(keyparam: string): void {
+  deleteItem(keyparam: string): string {
     // delete single item
     // the first slot is path or child
     // the second is key
-    const pathFirbase = 'CustomerInfo/' + keyparam;
-    this.af.object(pathFirbase)
-      .remove()
-      .then(() => alert('Successful for deleting item key'));
+    try{
+      const pathFirbase = 'CustomerInfo/' + keyparam;
+      this.af.object(pathFirbase)
+        .remove()
+        .then(() => alert('Successful for deleting user'));
+      return keyparam + 'has been delete'
+    }catch (err) {
+      return 'error'
+    }
 
   }
 
-  updateItem(keyparam: string): void {
+  updateItem(keyparam: string): string {
     // this.af.object('Item/{key}').update({'name': 'Jasmine' } );
-    this.onEdit();
-    this.datetime = this.currentdate.getDate() + '/'
-      + (this.currentdate.getMonth() + 1 ) + '/'
-      + this.currentdate.getFullYear() + ' @ '
-      + this.currentdate.getHours() + ':'
-      + this.currentdate.getMinutes() + ':'
-      + this.currentdate.getSeconds();
-    const pathFirebase = 'ProductInfo/' + keyparam;
-    this.af.object(pathFirebase)
-      .update({
+    try{
+      this.onEdit();
+      this.datetime = this.currentdate.getDate() + '/'
+        + (this.currentdate.getMonth() + 1 ) + '/'
+        + this.currentdate.getFullYear() + ' @ '
+        + this.currentdate.getHours() + ':'
+        + this.currentdate.getMinutes() + ':'
+        + this.currentdate.getSeconds();
+      const pathFirebase = 'ProductInfo/' + keyparam;
+      this.af.object(pathFirebase)
+        .update({
+          'City': this.model.City,
+
+          'Province': this.model.Province,
+
+
+          'CustomerFName': this.model.CustomerFName,
+          'CustomerLname': this.model.CustomerLName} )
+        .then(() => alert('Successful for Updating '));
+      this.editKey = null;
+      return  keyparam + 'has been update'
+    }catch (err) {
+      return 'error'
+    }
+  }
+
+  addItem(key: string): string {
+    //  อาจจะรับมาเป็น Product type ในหน้า html คงเป็น item  จสกนนั้นใน method ก็เขียนว่า 'CreatedDate' : productparam.CreatedDate
+
+    try{
+      this.datetime = this.currentdate.getDate() + '/'
+        + (this.currentdate.getMonth() + 1 ) + '/'
+        + this.currentdate.getFullYear() + ' @ '
+        + this.currentdate.getHours() + ':'
+        + this.currentdate.getMinutes() + ':'
+        + this.currentdate.getSeconds();
+
+      this.users.push({
         'City': this.model.City,
 
         'Province': this.model.Province,
 
 
+
         'CustomerFName': this.model.CustomerFName,
-        'CustomerLname': this.model.CustomerLName} )
-      .then(() => alert('Successful for Updating '));
-    this.editKey = null;
-  }
-
-  addItem(key: string): void {
-    //  อาจจะรับมาเป็น Product type ในหน้า html คงเป็น item  จสกนนั้นใน method ก็เขียนว่า 'CreatedDate' : productparam.CreatedDate
-
-    this.datetime = this.currentdate.getDate() + '/'
-      + (this.currentdate.getMonth() + 1 ) + '/'
-      + this.currentdate.getFullYear() + ' @ '
-      + this.currentdate.getHours() + ':'
-      + this.currentdate.getMinutes() + ':'
-      + this.currentdate.getSeconds();
-
-    this.users.push({
-      'City': this.model.City,
-
-      'Province': this.model.Province,
-
-
-
-      'CustomerFName': this.model.CustomerFName,
-      'CustomerLName': this.model.CustomerLName
-    })
-      .then(
-        () => alert('Successful for adding new Customer')
-      );
-    this.model.City = null;
-    this.model.Province = null;
-    this.model.CustomerLName = null;
-    this.model.CustomerFName= null;
-    this.onAdding();
+        'CustomerLName': this.model.CustomerLName
+      })
+        .then(
+          () => alert('Successful for adding new Customer')
+        );
+      this.model.City = null;
+      this.model.Province = null;
+      this.model.CustomerLName = null;
+      this.model.CustomerFName= null;
+      this.onAdding();
+      return 'user has been added'
+    }catch (err) {
+      return 'error'
+    }
   }
 
 
 
 
-  onEdit(): void {
+  onEdit(): string {
     // this method for change the state of isEdit that can disabled the button Edit or Save
     if (this.isEdit === true) {
       this.isEdit = false;
     } else {
       this.isEdit = true;
+      return 'isEdit = true'
     }
+    return 'isEdit = false'
   }
 
-  onAdding(): void {
+  onAdding(): string {
     // this method for enable the user to adding the new product
     if (this.isAdd === true) {
       this.isAdd = false;
     } else {
       this.isAdd = true;
+      return 'isAdd = true'
     }
+    return 'isAdd = false'
   }
-  openToEdit(keyparam: string): void {
+
+
+  openToEdit(keyparam: string): string {
     this.onEdit();
     this.editKey = keyparam;
+
+    return 'editKey=' + keyparam;
   }
 
 
